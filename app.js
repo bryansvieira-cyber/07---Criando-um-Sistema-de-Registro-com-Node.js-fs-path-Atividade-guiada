8754cb0c76312a8013584ad3d3f7b2db1a42afd4
Bryan <bryan.s.vieira@aluno.senai.br>
2026-04-13 16:36:29 -0300

    Git adicionado

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+const fs = require('fs');
 const path = require('path');
-console.log(__dirname)
+const caminhoArquivo = path.join(__dirname, 'registros', 'registros.txt');
+fs.writeFileSync(caminhoArquivo, '\nCliente Cliente: Felipe | Serviço: Driver, Cliente: Gabriela | Serviço: Backup, Cliente: Hugo | Serviço: Rede');
+console.log('Registro salvo.');
+
+caminhoArquivo
+const conteudo = fs.readFileSync(caminhoArquivo, 'utf-8');
+console.log('Histórico:');
+console.log(conteudo);
